a70855fd05b931d2dfadca8fbed6ed6790179672
codingWhale13 <nils.kiele@gmail.com>
2022-11-05 23:25:58 +0100

    Replace double quotes by single quotes

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
-  it "should exist at 'new_author_path' and render withour error" do
+describe 'New author page', type: :feature do
+  it 'should exist at "new_author_path" and render withour error' do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
 
-  it "should have text inputs for an author's first name, last name, and homepage" do
+  it 'should have text inputs for an author\'s first name, last name, and homepage' do
     visit new_author_path
     # these are the standard names given to inputs by the Rails form builder
     expect(page).to have_field('author[first_name]')
@@ -14,11 +14,11 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[homepage]')
   end
 
-  it "should save author from input fields" do
+  it 'should save author from input fields' do
     visit new_author_path
-    page.fill_in "author[first_name]",	with: "Edsger W."
-    page.fill_in "author[last_name]",	with: "Dijkstra" 
-    page.fill_in "author[homepage]",	with: "https://en.wikipedia.org/wiki/Edsger_W._Dijkstra" 
-    find("input[type=\"submit\"]").click
+    page.fill_in 'author[first_name]',	with: 'Edsger W.'
+    page.fill_in 'author[last_name]',	with: 'Dijkstra' 
+    page.fill_in 'author[homepage]',	with: 'https://en.wikipedia.org/wiki/Edsger_W._Dijkstra' 
+    find('input[type="submit"]').click
   end
 end
--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
-  it "should display the author's first name, last name and homepage" do
+describe 'New author page', type: :feature do
+  it 'should display the author\'s first name, last name and homepage' do
     @alan = FactoryBot.create :author
     visit author_path(@alan)
-    expect(page).to have_text("Alan")
-    expect(page).to have_text("Turing")
-    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+    expect(page).to have_text('Alan')
+    expect(page).to have_text('Turing')
+    expect(page).to have_text('http://wikipedia.de/Alan_Turing')
   end
 end
