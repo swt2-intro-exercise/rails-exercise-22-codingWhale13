d523c18a7a7f6c2fa59547d12212cdfffab160b9
codingWhale13 <nils.kiele@gmail.com>
2022-11-05 23:03:18 +0100

    Show author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,12 +1,32 @@
 class AuthorsController < ApplicationController
+    def index
+
+    end
+
+    def show
+        @author = Author.find(params[:id])
+    end
+    
     def new
 
     end
     
+    def edit
+
+    end
+
     def create
         @author = Author.new(author_params)
         @author.save
         redirect_to root_path, notice: "Success!"
+    end
+
+    def update
+
+    end
+
+    def destroy
+
     end
 
     private
