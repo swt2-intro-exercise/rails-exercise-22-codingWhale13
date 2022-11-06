604fb37d2834775904a130c9cbbbee0fbdf760c8
codingWhale13 <nils.kiele@gmail.com>
2022-11-06 12:09:21 +0100

    Allow deleting papers

--- spec/features/papers/index_paper_spec.rb
+++ spec/features/papers/index_paper_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 describe 'Paper index page', type: :feature do
-
-it 'should have a link to each paper\'s edit page' do
-    visit papers_path
-    Paper.all.each do |paper|
-      expect(page).to have_link('Edit', href: edit_paper_path(paper))
+    it 'should have a link to each paper\'s edit page' do
+        visit papers_path
+        Paper.all.each do |paper|
+            expect(page).to have_link('Edit', href: edit_paper_path(paper))
+        end
     end
-  end
+
+    it 'should have a link for each paper to delete it' do
+        visit papers_path
+        Paper.all.each do |paper|
+            expect(page).to have_link('Delete', href: paper_path(paper))
+        end
+    end
 end
